232070dbc83840e2f494e7c984699ce024d665fe
U+039b <lambda@0x39b.fr>
2016-07-13 10:29:59 +0200

    Add license

--- src/esther-compose.go
+++ src/esther-compose.go
@@ -1,3 +1,21 @@
+// This file is part of Esther-compose.
+//
+// Esther-compose is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Esther-compose is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Esther-compose.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Authored by U+039b <*[at]0x39b[dot]fr>
+// GPG: 29AF AD6D 8148 48FB 8147 3CEF E564 0E6E 5683 039D
+
 package main
 
 import ("bufio"
